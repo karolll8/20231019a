f2489de38342baee9539d8da0cbda551afc4dbde
karolll8 <128723904+karolll8@users.noreply.github.com>
2023-10-19 07:42:09 -0500

    Add files via upload

--- funciones.php
+++ funciones.php
@@ -17,8 +17,7 @@ function consulta(){
 function calcular(){
 
     $salida = 0;//inicializa la variable
-
     $conexion=mysqli_connect("localhost", "root", "root", "RUBLE_FORGOTAPP_PROYECT");//conexion con la base de datos
-
+    $sql= "SELECT 2+1 AS suma";//realiza una suma sql
     return $salida;//retornar al valor de la variable $salida
-}
+};
