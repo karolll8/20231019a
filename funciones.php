1fac791bd244e66ac7df043cb3c2a6fe822c07a3
karolll8 <128723904+karolll8@users.noreply.github.com>
2023-10-19 09:51:59 -0500

    Add files via upload

--- funciones.php
+++ funciones.php
@@ -20,7 +20,7 @@ function calcular()//se crea una nueva funcion (verbo infinitivo)
     $r= $conexion->query($sql); //ejecuta una consulta.
 
 
-while ($fila = mysqli_fetch_assoc($r)) {//recoge el recorset
+while ($fila = mysqli_fetch_assoc($r)) {//recoge el recordset
     $salida += $fila['suma'];//incrementa o acumula
 };
 
@@ -38,7 +38,7 @@ function calculo_v2()//se crea una funcion
     $sql = "SELECT 10 as n1, 20 as n2, (10 + 20) as sumas"; // Consulta SQL para calcular la suma de 10 y 20 y renombrarla como 'sumas'
     $resultado = $conexion->query($sql); // Ejecuta la consulta
 
-    while ($fila = $resultado->fetch_assoc()) { // Recoge el recorset
+    while ($fila = $resultado->fetch_assoc()) { // Recoge el recordset
         $salida += $fila['sumas']; // Incrementa o acumula
     };
 
@@ -55,7 +55,7 @@ function edad()//se crea una funcion
     $sql = "SELECT 30 as edad"; // Consulta SQL para obtener la edad 
     $resultado = $conexion->query($sql); // Ejecuta la consulta
 
-    while ($fila = $resultado->fetch_assoc()) { // Recoge el recorset
+    while ($fila = $resultado->fetch_assoc()) { // Recoge el recordset
         $salida = $fila['edad']; // Almacena la edad en la variable $salida
 
         $mensaje = ($salida >= 18) ? "Es mayor de edad" : "Es menor de edad";//se realiza un operador ternario (forma que reemplaza el if/also y es mas corta)
@@ -74,7 +74,7 @@ function conteo_usuario()//se crea una funcion
     $sql = "SELECT COUNT(*) AS cantidad FROM Usuarios"; // Consulta SQL para contar los usuarios
     $resultado = $conexion->query($sql); // Ejecuta la consulta
 
-    while ($fila = $resultado->fetch_assoc()) { // Recoge el recorset
+    while ($fila = $resultado->fetch_assoc()) { // Recoge el recordset
         $salida = $fila['cantidad']; // Obtiene el valor del recuento
     }
 
